8ae7b6b9b8716851a5e8b2f4b95c45fb64482281
DilanNestaresRamos <dnestaresramos@gmail.com>
2022-12-13 23:33:05 -0500

    Config Server

---
+++ src/main/java/com/ef/dilannestares/ConfigServer/EfDilanNestaresRamosConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.ef.dilannestares.ConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfDilanNestaresRamosConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfDilanNestaresRamosConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ef/dilannestares/ConfigServer/EfDilanNestaresRamosConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ef.dilannestares.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfDilanNestaresRamosConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
